0a5339c8628f74f3b0ed7444fe64d1704ca53172
denisolid <dkolisnichienko321@gmail.com>
2024-12-18 19:24:42 +0000

    background prepared

---
+++ src/components/AnimatedBackground.tsx
@@ -0,0 +1,5 @@
+export function AnimatedBackground() {
+  return (
+    <div className="absolute inset-0 -z-10 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 animate-gradient"></div>
+  );
+}
